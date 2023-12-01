40a87a192ec97934844138dc027fa01ae27e1e7b
Alex Bergeron <alexandrebergeron1@gmail.com>
2023-11-30 21:02:02 -0800

    init day1

--- day1/lib_test.ts
+++ day1/lib_test.ts
@@ -2,10 +2,10 @@ import { assertEquals } from "$std/assert/mod.ts";
 import Person, { sayHello } from "./lib.ts";
 
 Deno.test("sayHello function", () => {
-  const grace: Person = {
-    lastName: "Hopper",
-    firstName: "Grace",
-  };
+	const grace: Person = {
+		lastName: "Hopper",
+		firstName: "Grace",
+	};
 
-  assertEquals("Hello, Grace!", sayHello(grace));
+	assertEquals("Hello, Grace!", sayHello(grace));
 });
--- day1/main.ts
+++ day1/main.ts
@@ -1,8 +1,8 @@
 import Person, { sayHello } from "./lib.ts";
 
 const ada: Person = {
-  firstName: "Ada",
-  lastName: "Lovelace",
+	firstName: "Ada",
+	lastName: "Lovelace",
 };
 
 console.log(sayHello(ada));

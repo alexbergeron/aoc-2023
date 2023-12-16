43fbff411c0741cfbe8b836d58c3a3379e8b9fa2
Alexandre Bergeron <alexbergeron@users.noreply.github.com>
2023-12-16 01:23:10 -0500

    Day 16

--- day16/main.ts
+++ day16/main.ts
@@ -4,13 +4,13 @@
 import { Grid, parseGrid } from "../lib/grid.ts";
 import { assert } from "$std/assert/assert.ts";
 import { delay } from "$std/async/delay.ts";
-import { List, Range, Record, RecordOf, Set } from "$immutable/mod.ts";
+import { List, OrderedSet, Range, Record, RecordOf, Set } from "$immutable/mod.ts";
 import * as R from "$ramda/mod.ts";
 
 type Position = List<number>;
 type BeamProps = { position: Position; direction: Position };
-const Beam = Record({position: List([0,0]), direction: List([0, 1])})
-type Beam = RecordOf<BeamProps>
+const Beam = Record({ position: List([0, 0]), direction: List([0, 1]) });
+type Beam = RecordOf<BeamProps>;
 
 async function readInput(file: string): Promise<Grid> {
 	const out = await Deno.readTextFile(`./day16/${file}`);
@@ -21,11 +21,10 @@ async function moveBeam(
 	beam: Beam,
 	grid: Grid,
 	visited: Set<Beam> = Set(),
-	next: List<Beam> = List(),
+	next: OrderedSet<Beam> = OrderedSet(),
 ): Promise<Set<Beam>> {
-
 	const [row, col] = beam.position;
-	await delay(1);
+	await delay(0);
 	if (visited.contains(beam)) {
 		const head = next.first();
 
@@ -42,13 +41,13 @@ async function moveBeam(
 	}
 
 	const [dirRow, dirCol] = beam.direction;
-	const newNext = next.withMutations((tempNext) => {
+	const nextCandidates = Set<Beam>().withMutations((tempNext) => {
 		switch (grid.get(row)?.get(col)) {
 			case ".": {
 				const nextRow = row + dirRow;
 				const nextCol = col + dirCol;
 				if (nextRow >= 0 && nextRow < grid.size && nextCol >= 0 && nextCol < grid.get(0, List()).size) {
-					tempNext.push(Beam({ position: List([nextRow, nextCol]), direction: beam.direction }));
+					tempNext.add(Beam({ position: List([nextRow, nextCol]), direction: beam.direction }));
 				}
 				break;
 			}
@@ -56,7 +55,7 @@ async function moveBeam(
 				const nextRow = row + dirCol;
 				const nextCol = col + dirRow;
 				if (nextRow >= 0 && nextRow < grid.size && nextCol >= 0 && nextCol < grid.get(0, List()).size) {
-					tempNext.push(Beam({ position: List([nextRow, nextCol]), direction: List([dirCol, dirRow]) }));
+					tempNext.add(Beam({ position: List([nextRow, nextCol]), direction: List([dirCol, dirRow]) }));
 				}
 				break;
 			}
@@ -64,7 +63,7 @@ async function moveBeam(
 				const nextRow = row - dirCol;
 				const nextCol = col - dirRow;
 				if (nextRow >= 0 && nextRow < grid.size && nextCol >= 0 && nextCol < grid.get(0, List()).size) {
-					tempNext.push(Beam({ position: List([nextRow, nextCol]), direction: List([-dirCol, -dirRow]) }));
+					tempNext.add(Beam({ position: List([nextRow, nextCol]), direction: List([-dirCol, -dirRow]) }));
 				}
 				break;
 			}
@@ -74,18 +73,18 @@ async function moveBeam(
 					const nextRow = row + dirRow;
 					const nextCol = col + dirCol;
 					if (nextRow >= 0 && nextRow < grid.size && nextCol >= 0 && nextCol < grid.get(0, List()).size) {
-						tempNext.push(Beam({ position: List([nextRow, nextCol]), direction: beam.direction }));
+						tempNext.add(Beam({ position: List([nextRow, nextCol]), direction: beam.direction }));
 					}
 				} else {
 					// Splits horizontally
 					const nextRow = row;
 					let nextCol = col - 1;
 					if (nextRow >= 0 && nextRow < grid.size && nextCol >= 0 && nextCol < grid.get(0, List()).size) {
-						tempNext.push(Beam({ position: List([nextRow, nextCol]), direction: List([0, -1]) }));
+						tempNext.add(Beam({ position: List([nextRow, nextCol]), direction: List([0, -1]) }));
 					}
 					nextCol = col + 1;
 					if (nextRow >= 0 && nextRow < grid.size && nextCol >= 0 && nextCol < grid.get(0, List()).size) {
-						tempNext.push(Beam({ position: List([nextRow, nextCol]), direction: List([0, 1]) }));
+						tempNext.add(Beam({ position: List([nextRow, nextCol]), direction: List([0, 1]) }));
 					}
 				}
 				break;
@@ -96,24 +95,26 @@ async function moveBeam(
 					const nextRow = row + dirRow;
 					const nextCol = col + dirCol;
 					if (nextRow >= 0 && nextRow < grid.size && nextCol >= 0 && nextCol < grid.get(0, List()).size) {
-						tempNext.push(Beam({ position: List([nextRow, nextCol]), direction: beam.direction }));
+						tempNext.add(Beam({ position: List([nextRow, nextCol]), direction: beam.direction }));
 					}
 				} else {
 					// Splits vertically
 					let nextRow = row - 1;
 					const nextCol = col;
 					if (nextRow >= 0 && nextRow < grid.size && nextCol >= 0 && nextCol < grid.get(0, List()).size) {
-						tempNext.push(Beam({ position: List([nextRow, nextCol]), direction: List([-1, 0]) }));
+						tempNext.add(Beam({ position: List([nextRow, nextCol]), direction: List([-1, 0]) }));
 					}
 					nextRow = row + 1;
 					if (nextRow >= 0 && nextRow < grid.size && nextCol >= 0 && nextCol < grid.get(0, List()).size) {
-						tempNext.push(Beam({ position: List([nextRow, nextCol]), direction: List([1, 0]) }));
+						tempNext.add(Beam({ position: List([nextRow, nextCol]), direction: List([1, 0]) }));
 					}
 				}
 				break;
 			}
 		}
 	});
+
+	const newNext = next.concat(nextCandidates.filter((beam) => !visited.contains(beam)));
 
 	const head = newNext.first();
 
@@ -139,23 +140,27 @@ async function run1(inputPath: string): Promise<number> {
 async function run2(inputPath: string): Promise<number> {
 	// IT CAN BE BRUTE FORCED!!!
 	const grid = await readInput(inputPath);
-	const height = grid.size
-	const width = grid.get(0)?.size ?? 0
-	const top = Range(0, width).map((col) => moveBeam(Beam({position: List([0, col]), direction: List([1, 0])}), grid))
-	const bottom = Range(0, width).map((col) => moveBeam(Beam({position: List([height - 1, col]), direction: List([-1, 0])}), grid))
-	const left = Range(0, height).map((row) => moveBeam(Beam({position: List([row, 0]), direction: List([0, 1])}), grid))
-	const right = Range(0, height).map((row) => moveBeam(Beam({position: List([row, width - 1]), direction: List([0, -1])}), grid))
-	const all = top.concat(bottom, left, right)
-	const max = all.reduce(async (currMaxPromise, nextPromise, idx) => {
-		console.log(idx)
-		await delay(10)
-		const currMax = await currMaxPromise
-		const next = await nextPromise
-		const nextPos = next.map(R.prop('position'))
-		return Math.max(currMax, nextPos.size)
+	const height = grid.size;
+	const width = grid.get(0)?.size ?? 0;
+	const top = Range(0, width).map((col) => moveBeam(Beam({ position: List([0, col]), direction: List([1, 0]) }), grid));
+	const bottom = Range(0, width).map((col) =>
+		moveBeam(Beam({ position: List([height - 1, col]), direction: List([-1, 0]) }), grid)
+	);
+	const left = Range(0, height).map((row) =>
+		moveBeam(Beam({ position: List([row, 0]), direction: List([0, 1]) }), grid)
+	);
+	const right = Range(0, height).map((row) =>
+		moveBeam(Beam({ position: List([row, width - 1]), direction: List([0, -1]) }), grid)
+	);
+	const all = top.concat(bottom, left, right);
 
-	}, Promise.resolve(0))
-	return await max;
+	let max = 0;
+	for (const p of all) {
+		const next = await p;
+		const nextPos = next.map(R.prop("position"));
+		max = Math.max(max, nextPos.size);
+	}
+	return max;
 }
 
 const expected1 = 46;
@@ -165,7 +170,7 @@ console.log("OK");
 const result1 = await run1("input");
 console.log(result1);
 
-const expected2 = 51	
+const expected2 = 51;
 const test2 = await run2("test");
 assert(test2 == expected2, `${test2} != ${expected2}`);
 console.log("OK");
